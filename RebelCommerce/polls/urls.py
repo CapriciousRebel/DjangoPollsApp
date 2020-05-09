732032fc98106d7cbf18f3d408f50976f5f6d0b7
Smith <codingfreak98@Smiths-MacBook-Pro.local>
2020-05-09 16:23:32 +0530

    complete polls app

--- RebelCommerce/polls/urls.py
+++ RebelCommerce/polls/urls.py
@@ -2,16 +2,17 @@ from django.urls import path
 
 from . import views
 
+# set namespace polls
+app_name = 'polls'
 
 # Map urls on polls/ to their views
-
 urlpatterns = [
     # polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # polls/<question_id>/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # polls/<question_id>/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # polls/<question_id>/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
